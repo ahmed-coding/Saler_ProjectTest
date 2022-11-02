660454f5d92451aa0c381e32a59b23827b8a268e
AM2299 <ahmed.128hemzh@gmail.com>
2022-11-03 00:13:20 +0300

    Complate Prodect Page <22>

--- Forms/frm_prodect.cs
+++ Forms/frm_prodect.cs
@@ -9,26 +9,41 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using DevExpress.XtraEditors.Repository;
+using DevExpress.XtraGrid.Views.Grid;
 
 namespace Saler_Project.Forms
 {
    
     public partial class frm_prodect : Saler_Project.Forms.frm_master
     {
-       public enum ProdectType
+        //start Mempers
+        public enum ProdectType
         {
             Inventory,
             Servise,
         }
         Scr.Prodect prodect;
         RepositoryItemLookUpEdit lookUpEdit = new RepositoryItemLookUpEdit();
+        Scr.DBDataContext dBs = new Scr.DBDataContext();
+        Scr.Prodect_unit unit = new Scr.Prodect_unit();
+
+        class ValueAndID
+        {
+            public int id { get; set; }
+            public string name { get; set; }
+        }
+
+
+        //End Mempers
+
+        //End Events
         public frm_prodect()
         {
             InitializeComponent();
             New();
             this.Load += Frm_prodect_Load;
         }
-
+        
         private void Frm_prodect_Load(object sender, EventArgs e)
         {
             refreshData();
@@ -44,7 +59,7 @@ namespace Saler_Project.Forms
 
             gridView1.OptionsView.ShowGroupPanel = false;
             gridView1.OptionsView.NewItemRowPosition = DevExpress.XtraGrid.Views.Grid.NewItemRowPosition.Top;
-            Scr.Prodect_unit unit = new Scr.Prodect_unit();
+            
             gridView1.Columns[nameof(unit.prodect_id)].Visible = false;
             gridView1.Columns[nameof(unit.id)].Visible = false;
 
@@ -61,14 +76,67 @@ namespace Saler_Project.Forms
             gridView1.Columns[nameof(unit.sellDiscount)].ColumnEdit = calcEdit;
             gridView1.Columns[nameof(unit.unit_id)].ColumnEdit = lookUpEdit;
 
+            gridView1.Columns[nameof(unit.sellPress)].Caption = "سعر البيع ";
+            gridView1.Columns[nameof(unit.sellDiscount)].Caption = "الخصم";
+            gridView1.Columns[nameof(unit.vactor)].Caption = "الكمية ";
+            gridView1.Columns[nameof(unit.barrCode)].Caption = "الكود";
+            gridView1.Columns[nameof(unit.buyPress)].Caption = "سعر الشراء";
+            gridView1.Columns[nameof(unit.unit_id)].Caption = "الوحدة";
+
             lookUpEdit.NullText = "";
             lookUpEdit.ValueMember = "id";
-            lookUpEdit.DisplayMember = "name";
-            refreshData();
+            lookUpEdit.DisplayMember = "name";            
 
             lookUpEdit.TextEditStyle = DevExpress.XtraEditors.Controls.TextEditStyles.Standard;
-
             lookUpEdit.ProcessNewValue += LookUpEdit_ProcessNewValue;
+
+            gridView1.ValidateRow += GridView1_ValidateRow;
+            gridView1.InvalidRowException += GridView1_InvalidRowException;
+            gridView1.CellValueChanged += GridView1_CellValueChanged;
+            gridView1.FocusedRowChanged += GridView1_FocusedRowChanged;
+        }
+
+        private void GridView1_FocusedRowChanged(object sender, DevExpress.XtraGrid.Views.Base.FocusedRowChangedEventArgs e)
+        {
+           
+            
+                gridView1.Columns[nameof(unit.vactor)].OptionsColumn.AllowEdit = !(e.FocusedRowHandle ==0);
+            
+        }
+
+        private void GridView1_CellValueChanged(object sender, DevExpress.XtraGrid.Views.Base.CellValueChangedEventArgs e)
+        {
+
+        }
+
+        private void GridView1_InvalidRowException(object sender, DevExpress.XtraGrid.Views.Base.InvalidRowExceptionEventArgs e)
+        {
+            e.ExceptionMode = DevExpress.XtraEditors.Controls.ExceptionMode.NoAction;
+        }
+
+        private void GridView1_ValidateRow(object sender, DevExpress.XtraGrid.Views.Base.ValidateRowEventArgs e)
+        {
+            var row = e.Row as Scr.Prodect_unit;
+            var view = sender as GridView;
+            if (row == null) return;
+
+            if(row.vactor <= 1 && e.RowHandle !=0)
+            {
+                e.Valid = false;
+                gridView1.SetColumnError(gridView1.Columns[nameof(row.vactor)],"يجب ان تكون القيمة اكبر من واحد ");
+            }
+            if (row.unit_id <=0)
+            {
+                e.Valid = false;
+                gridView1.SetColumnError(gridView1.Columns[nameof(row.unit_id)], errorText);
+            }
+            if (checkBarrcode(row.barrCode, prodect.id))
+            {
+                e.Valid = false;
+                gridView1.SetColumnError(gridView1.Columns[nameof(row.barrCode)], "هذا الكود موجود بافعل");
+            }
+
+
         }
 
         private void LookUpEdit_ProcessNewValue(object sender, DevExpress.XtraEditors.Controls.ProcessNewValueEventArgs e)
@@ -86,28 +154,40 @@ namespace Saler_Project.Forms
             }
         }
 
-        class ValueAndID
-        {
-            public int id  { get; set; }
-            public string name { get; set; }
-        }
 
         private void LookUpCatedory_ProcessNewValue(object sender, DevExpress.XtraEditors.Controls.ProcessNewValueEventArgs e)
         {
            
         }
+        //End Events
 
+
+        //start methods
         public override void New()
         {
-            prodect = new Scr.Prodect();
+            prodect = new Scr.Prodect()
+            {
+                code= getNewCode()
+            } ;
 
             base.New();
+            var data = gridView1.DataSource as BindingList<Scr.Prodect_unit>;
+            var db =new Scr.DBDataContext();
+            if (db.Units.Count() == 0)
+            {
+                db.Units.InsertOnSubmit(new Scr.Unit() { name = "قطعه" } );
+                db.SubmitChanges();
+                refreshData();
+            }
+            data.Add(new Scr.Prodect_unit() { vactor = 1, unit_id = db.Units.First().id,barrCode =getNewBarrCode() });
         }
         Byte[] getByteFromImage(Image img)
         {
             using (MemoryStream stream = new MemoryStream() )
             {
-               
+
+                if (img == null)
+                    return stream.ToArray();
 
                 try
                 {
@@ -118,9 +198,13 @@ namespace Saler_Project.Forms
 
                     return stream.ToArray();
                 }
+               
+
             }
         }
-        Scr.DBDataContext dBs = new Scr.DBDataContext();
+
+       
+        
         public override void GetData()
         {
             txtCode.Text = prodect.code;
@@ -186,6 +270,7 @@ namespace Saler_Project.Forms
                 txtCode.ErrorText = "هذا الكود موجود مسبقآ";
                 return false;
             }
+
             return true;
         }
         public override void Save()
@@ -206,7 +291,7 @@ namespace Saler_Project.Forms
             foreach (var item in data)
             {
                 item.prodect_id = prodect.id;
-                if (string.IsNullOrEmpty(item.barrCode))
+                if (item.barrCode == string.Empty || item.barrCode == null)
                 {
                     item.barrCode = "";
                 }
@@ -218,7 +303,7 @@ namespace Saler_Project.Forms
         public override void refreshData()
         {
             using (Scr.DBDataContext db = new Scr.DBDataContext()){
-                //lookUpCatedory.Properties.DataSource = db.Prodect_Categories.Where(x => db.Prodect_Categories.Where(y => y.parent_id == x.id).Count() == 0).ToList();
+               
                 lookUpCatedory.Properties.DataSource = db.Prodect_Categories.ToList();
                 lookUpEdit.DataSource = db.Units.ToList();
 
@@ -227,5 +312,64 @@ namespace Saler_Project.Forms
 
             base.refreshData();
         }
+        string getNewCode()
+        {
+            string maxCode;
+            using (Scr.DBDataContext db = new Scr.DBDataContext())
+            {
+                maxCode = db.Prodects.Select(x => x.code).Max();
+            }
+
+            return getNextCode(maxCode);
+
+        }
+        string getNewBarrCode()
+        {
+            string maxCode;
+            using (Scr.DBDataContext db = new Scr.DBDataContext())
+            {
+                maxCode = db.Prodect_units.Select(x => x.barrCode).Max();
+            }
+
+            return getNextCode(maxCode);
+
+        }
+
+        string getNextCode( string code)
+        {
+            if (code == string.Empty || code == null) return "1";
+
+            string str1 = "";
+            foreach (char item in code)
+            {
+                str1 = char.IsDigit(item) ? str1 + item.ToString() : "";
+            }
+
+            if (str1 == string.Empty) return code + "1";
+
+
+            string str2 = str1.Insert(0, "1");
+            str2 = (Convert.ToUInt32(str2)+1).ToString();
+
+         string  str3 = str2[0] == '1' ? str2.Remove(0, 1) : str2.Remove(0, 1).Insert(0,"1");
+
+            int index = code.LastIndexOf(str1);
+            code = code.Remove(index);
+
+            code = code.Insert(index, str3);
+
+            return code;
+        }
+
+        Boolean checkBarrcode(string barrCode  ,int id)
+        {
+            using (Scr.DBDataContext db = new Scr.DBDataContext())
+            {
+              return  db.Prodect_units.Select(x => x.barrCode == barrCode && x.prodect_id != id ).Count() > 0;
+            }
+
+        }
+
+        //End methods
     }
 }
